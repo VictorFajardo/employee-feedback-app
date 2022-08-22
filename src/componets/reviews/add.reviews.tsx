704ba530f39f3e7f3c48856d59f51cf1092c5c9d
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-08-22 01:06:10 -0400

    Updating the review components with Material UI

--- src/componets/reviews/add.reviews.tsx
+++ src/componets/reviews/add.reviews.tsx
@@ -1,14 +1,21 @@
 // React components
-import { useState } from "react";
+import { SetStateAction, SyntheticEvent, useState } from "react";
 // Redux components
 import { useAppSelector, useAppDispatch } from '../../hooks';
 import { addReview } from "../../features/reviews/reviewsSlice";
 import { selectEmployees, updateEmployee } from "../../features/employees/employeesSlice";
 // Api components
 import { createReviewApi, updateUserApi, getReviewApi } from "../../utilities/firebase";
+// Chidren components
+import Title from "../elements/title";
+// Interface components
+import { EmployeeInterface } from "../../interfaces";
 // Material components
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Autocomplete from "@mui/material/Autocomplete";
 
 interface AddReviewsProps {
   closeMethod: () => void,
@@ -17,19 +24,23 @@ interface AddReviewsProps {
 const AddReviews: React.FC<AddReviewsProps> = ({ closeMethod }) => {
   const dispatch = useAppDispatch();
   const employees = useAppSelector(selectEmployees); // Select all the epmloyees to build the employee list
+  const options = ['Option 1', 'Option 2'];
 
   //TODO Select the employee and the reviewer directly from the dropdown boxes
   const [employeeId, setEmployeeId] = useState('');
   const [reviewerId, setReviewerId] = useState('');
+  const [value, setValue] = useState<EmployeeInterface | null>(null);
+  const [inputValue, setInputValue] = useState('');
 
-  const handleChangeEmployee = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleChangeEmployee = (event: SyntheticEvent<Element, Event>) => {
     //TODO Select the employee and the reviewer directly from the dropdown boxes
-    setEmployeeId(event.target.value);
+    // setEmployeeId(event.target.value);
+    console.log(event);
   };
 
-  const handleChangeReviewer = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleChangeReviewer = (event: SyntheticEvent<Element, Event>) => {
     //TODO Select the employee and the reviewer directly from the dropdown boxes
-    setReviewerId(event.target.value);
+    // setReviewerId(event.target.value);
   };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -64,29 +75,49 @@ const AddReviews: React.FC<AddReviewsProps> = ({ closeMethod }) => {
   };
 
   return (
-    <>
-      <h2>Add a new review</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="firstName">Employee </label>
-        <select value={employeeId} onChange={handleChangeEmployee}>
-          {!employeeId && <option value=''></option>}
-          {employees.map((employee) => (
-            <option key={employee.id} value={employee.id}>{employee.lastName}, {employee.firstName}</option>
-          ))}
-        </select>
-        <br />
-        <label htmlFor="lastName">Reviewer </label>
-        <select value={reviewerId} onChange={handleChangeReviewer}>
-          {!reviewerId && <option value=''></option>}
-          {employees.map((employee) => (
-            <option key={employee.id} value={employee.id}>{employee.lastName}, {employee.firstName}</option>
-          ))}
-        </select>
-        <Box sx={{ flexGrow: 1, display: "flex", justifyContent: 'center' }}>
-          <Button type="submit" sx={{ my: 2, ml: 2, display: 'flex' }} variant="outlined">ADD</Button> <Button onClick={closeMethod} sx={{ my: 2, ml: 2, display: 'flex' }} variant="contained">CANCEL</Button>
-        </Box>
-      </form>
-    </>
+    <Container>
+    <Title text='Add a new review' align={'column'} />
+    <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+      <Autocomplete
+        sx={{ margin: '16px 0 8px' }}
+        autoSelect={true}
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue!);
+        }}
+        inputValue={inputValue}
+        onInputChange={(event, newInputValue) => {
+          setInputValue(newInputValue!);
+        }}
+        options={employees}
+        getOptionLabel={(option) => `${option.firstName} ${option.lastName}`}
+        renderInput={(params) => <TextField {...params} label="Select employee" />}
+      />
+      <Box sx={{ flexGrow: 1, display: "flex", justifyContent: 'center' }}>
+        <Button type="submit" sx={{ my: 2, ml: 2, display: 'flex' }} variant="outlined">ADD</Button> <Button onClick={closeMethod} sx={{ my: 2, ml: 2, display: 'flex' }} variant="contained">CANCEL</Button>
+      </Box>
+    </Box>
+  </Container>
+    // <>
+    //   <h2>Add a new review</h2>
+    //   <form onSubmit={handleSubmit}>
+    //     <label htmlFor="firstName">Employee </label>
+    //     <select value={employeeId} onChange={handleChangeEmployee}>
+    //       {!employeeId && <option value=''></option>}
+    //       {employees.map((employee) => (
+    //         <option key={employee.id} value={employee.id}>{employee.lastName}, {employee.firstName}</option>
+    //       ))}
+    //     </select>
+    //     <br />
+    //     <label htmlFor="lastName">Reviewer </label>
+    //     <select value={reviewerId} onChange={handleChangeReviewer}>
+    //       {!reviewerId && <option value=''></option>}
+    //       {employees.map((employee) => (
+    //         <option key={employee.id} value={employee.id}>{employee.lastName}, {employee.firstName}</option>
+    //       ))}
+    //     </select>
+    //   </form>
+    // </>
   )
 }
 
